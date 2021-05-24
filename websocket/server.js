f5c00d5176c7598dac4135072011ee77894f42df
ChahedHassine <65560028+ChahedHassine@users.noreply.github.com>
2021-05-24 22:28:33 +0100

    Add files via upload

--- websocket/server.js
+++ websocket/server.js
@@ -12,28 +12,18 @@ const funFacts = [
     'votre nom est',
     'votre pénom est',
 ];
-
-// lancer la fonction connection lorsque un client se connecte
-// a notre server ws
-wss.on('connection', function connection(ws) {
-    // dans la fonction de l'event connection
-    // on spécifie la logique websocket
+wss.on('connection', function connection(ws) { 
     console.log('New client connected');
-    // lancer la methode dans setinterval chaque X temps
     setInterval(() => {
-        // choisir un element de funFacts par hasard
+        
         const index = Math.floor(Math.random() * funFacts.length);
-        // retourner au client funFacts[index]
+        
         ws.send(funFacts[index]);
     }, 10 * 1000);
-    // au cas ou on veut récuperer les données envoyés
-    // par le client selon un evenement (e.g. message)
     ws.on('message', function incoming(message) {
         console.log(`received ${message}`);
-        // console.log('received ' + message);
     });
 });
 
 app.get('/', (req, res) => res.send('hello world')),
-
-    server.listen(3000, () => console.log('listening on port : 3000'));
+server.listen(3000, () => console.log('listening on port : 3000'));
